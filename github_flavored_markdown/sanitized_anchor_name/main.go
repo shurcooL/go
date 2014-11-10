f7af10954f2cb6deea7cf242c1ecaefbb79cc872
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-10 01:38:02 -0800

    sanitized_anchor_name: Add Context support.
    Rough initial version, could use some clean up.

--- github_flavored_markdown/sanitized_anchor_name/main.go
+++ github_flavored_markdown/sanitized_anchor_name/main.go
@@ -1,7 +1,10 @@
 // Package sanitized_anchor_name provides a func to create sanitized anchor names.
 package sanitized_anchor_name
 
-import "unicode"
+import (
+	"fmt"
+	"unicode"
+)
 
 // Create returns a sanitized anchor name for the given text.
 func Create(text string) string {
@@ -15,4 +18,39 @@ func Create(text string) string {
 		}
 	}
 	return string(anchorName)
+}
+
+// A Context specifies the supporting context for a generating anchor names.
+// All generated anchor names within the same context are guaranteed to be unique.
+type Context struct {
+	m map[string]uint
+}
+
+func NewContext() *Context {
+	return &Context{m: make(map[string]uint)}
+}
+
+func (ctxt *Context) Create(text string) string {
+	// TODO: Clean up this code.
+	var anchorName string
+
+	originalAnchorName := Create(text)
+	anchorName = originalAnchorName
+
+TryAgain:
+	if ctxt.m[originalAnchorName] != 0 {
+		anchorName = originalAnchorName + fmt.Sprintf("-%d", ctxt.m[originalAnchorName]+1)
+	}
+
+	if ctxt.m[anchorName] != 0 {
+		ctxt.m[originalAnchorName] = ctxt.m[originalAnchorName] + 1
+		goto TryAgain
+	} else {
+		ctxt.m[originalAnchorName] = ctxt.m[originalAnchorName] + 1
+	}
+	if anchorName != originalAnchorName {
+		ctxt.m[anchorName] = ctxt.m[anchorName] + 1
+	}
+
+	return anchorName
 }
--- github_flavored_markdown/sanitized_anchor_name/main_test.go
+++ github_flavored_markdown/sanitized_anchor_name/main_test.go
@@ -14,3 +14,54 @@ func ExampleCreate() {
 	// Output:
 	//this-is-a-header
 }
+
+func ExampleCollisions() {
+	var anchorNames string
+
+	context := sanitized_anchor_name.NewContext()
+	anchorNames += context.Create("Header") + "\n"
+	anchorNames += context.Create("Header") + "\n"
+	anchorNames += context.Create("Header") + "\n"
+
+	fmt.Println(anchorNames)
+
+	// Output:
+	//header
+	//header-2
+	//header-3
+	//
+}
+
+func ExampleCollisions2() {
+	var anchorNames string
+
+	context := sanitized_anchor_name.NewContext()
+	anchorNames += context.Create("Header") + "\n"
+	anchorNames += context.Create("Header 2") + "\n"
+	anchorNames += context.Create("Header") + "\n"
+
+	fmt.Println(anchorNames)
+
+	// Output:
+	//header
+	//header-2
+	//header-3
+	//
+}
+
+func ExampleCollisions3() {
+	var anchorNames string
+
+	context := sanitized_anchor_name.NewContext()
+	anchorNames += context.Create("Header") + "\n"
+	anchorNames += context.Create("Header") + "\n"
+	anchorNames += context.Create("Header 2") + "\n"
+
+	fmt.Println(anchorNames)
+
+	// Output:
+	//header
+	//header-2
+	//header-2-2
+	//
+}
